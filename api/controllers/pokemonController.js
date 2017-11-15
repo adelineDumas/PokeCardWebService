24b41de4e2362bb1bd940be6749849805446dc7c
Thomas <thomas.chaboud@etu.univ-lyon1.fr>
2017-11-15 11:41:34 +0100

    Ajout de routes + ajout de l\’url de l\'image d'un pokemon dans les infos du JSON du pokedex

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -3,7 +3,35 @@ var https = require('https');
 
 exports.pokedex = function(req, res) {
 
-	var options = "https://pokeapi.co/api/v2/pokedex/1/";
+	var options = "https://pokeapi.co/api/v2/pokedex/1/"; //pokedex national
+
+	var data = "";
+
+	var request = https.get(options, (result) => {
+		result.on('data', (d) => {
+			data += d;
+		});
+		result.on('end', function() {
+			var infoPokemon = JSON.parse(data);
+			for(var i=0;i<721;i++){
+				var id_pkmn = i+1;
+				infoPokemon.pokemon_entries[i].pokemon_species["url_img"] = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+id_pkmn+".png";
+			}
+			res.json(infoPokemon);
+		});
+	});
+
+	request.on('error', (e) => {
+		console.error(e);
+	});
+
+	request.end();
+}
+
+exports.pokemon = function(req, res) {
+
+	var pokemonId = params.req.pokemonId;
+	var options = "https://pokeapi.co/api/v2/pokemon/"+pokemonId+"/";
 
 	var data = "";
 
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,16 +1,13 @@
 'use strict';
 module.exports = function(app) {
 	var pokemonController = require('../controllers/pokemonController');
-	var authController = require('../controllers/authController');
+	var authController = require('../controllers/userController');
 
 	app.route('/pokedex').get(pokemonController.pokedex);
-
-	app.route('/init/:userId').get(authController.init);
-
-	app.route('/login').post(authController.login);
-
-	app.route('/signup').post(authController.signup);
-
-	app.route('/verify').post(authController.verify);
+	app.route('/pokemon/:pokemonId').get(pokemonController.pokedex);
+	//app.route('/verifylogin').post(authController.login);
+	//app.route('/signup').post(authController.signup);
+	//app.route('/collection').get(pokemonController.collection);
+	//app.route('/listeamis').get(pokemonController.amis);
 
 };
